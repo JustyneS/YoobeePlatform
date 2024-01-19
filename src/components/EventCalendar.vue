c828e50f7e937c93e086fab4ae8b6bd2d3e924fd
Josh Hendrie <Josh.Hendrie101@gmail.com>
2024-01-19 21:59:30 +1300

    Delete function

--- src/components/EventCalendar.vue
+++ src/components/EventCalendar.vue
@@ -6,11 +6,13 @@ import timeGridPlugin from '@fullcalendar/timegrid'
 import listPlugin from '@fullcalendar/list'
 import interactionPlugin from '@fullcalendar/interaction'
 
-const id = ref(0)
-const selectedEvent = ref(null)
-const newTitle = ref('')
-const storedEvents = JSON.parse(localStorage.getItem('calendarEvents')) || [];
+// Define reactive variables
+const id = ref(0) // Counter for event IDs
+const selectedEvent = ref(null) // Currently selected event
+const newTitle = ref('') // Title for the new event or modified event
+const storedEvents = JSON.parse(localStorage.getItem('calendarEvents')) || []; // Array to store calendar events
 
+// Define options for FullCalendar
 const options = reactive({
     plugins: [dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin],
     initialView: 'dayGridMonth',
@@ -19,44 +21,49 @@ const options = reactive({
         center: 'title',
         right: 'dayGridMonth,dayGridWeek,listDay'
     },
-    editable: true,
-    selectable: true,
-    weekends: true,
-    events: storedEvents,
+    selectable: true, // Allow users to select dates
+    weekends: true, // Show weekends in the calendar
+    events: storedEvents, // Initial events loaded from local storage
 
+    // Callback function when a date range is selected
     select: (arg) => {
-    id.value = id.value + 1;
+        id.value = id.value + 1; // Increment the event ID counter
 
-    const cal = arg.view.calendar;
-    cal.unselect();
-    const newEvent = {
-        id: `${id.value}`,
-        title: `New Event ${id.value}`,
-        start: arg.start,
-        end: arg.end,
-    };
+        const cal = arg.view.calendar;
+        cal.unselect();
 
-    cal.addEvent(newEvent);
-    
-    // Save the events to local storage
-    const storedEvents = JSON.parse(localStorage.getItem('calendarEvents')) || [];
-    storedEvents.push(newEvent);
-    localStorage.setItem('calendarEvents', JSON.stringify(storedEvents));
-},
+        // Create a new event object
+        const newEvent = {
+            id: `${id.value}`,
+            title: `New Event ${id.value}`,
+            start: arg.start,
+            end: arg.end,
+            allDay: true,
+        };
 
-    eventClick: (arg) => {
-        selectedEvent.value = arg.event
-        newTitle.value = arg.event.title
+        cal.addEvent(newEvent); // Add the new event to the calendar
+
+        // Save the events to local storage
+        const storedEvents = JSON.parse(localStorage.getItem('calendarEvents')) || [];
+        storedEvents.push(newEvent);
+        localStorage.setItem('calendarEvents', JSON.stringify(storedEvents));
     },
 
+    // Callback function when an event is clicked
+    eventClick: (arg) => {
+        selectedEvent.value = arg.event; // Set the selected event
+        newTitle.value = arg.event.title; // Set the title for editing
+    },
+
+    // Callback function when an event is changed (dragged or resized)
     eventChange: (arg) => {
         console.log(`Event ${arg.event.title} was changed!`)
     }
 })
 
+// Function to save changes made to the selected event
 const saveChanges = () => {
     if (selectedEvent.value) {
-        // Update the title of the selected event
         selectedEvent.value.setProp('title', newTitle.value);
 
         // Get the current events from local storage
@@ -68,6 +75,28 @@ const saveChanges = () => {
         if (eventIndex !== -1) {
             // Update the title of the selected event in the stored events array
             storedEvents[eventIndex].title = newTitle.value;
+
+            // Save the modified events array back to local storage
+            localStorage.setItem('calendarEvents', JSON.stringify(storedEvents));
+        }
+
+        // Clear the selected event and newTitle values
+        selectedEvent.value = null;
+        newTitle.value = '';
+    }
+};
+
+const deleteEvent = () => {
+    if (selectedEvent.value) {
+        // Get the current events from local storage
+        const storedEvents = JSON.parse(localStorage.getItem('calendarEvents')) || [];
+
+        // Find the index of the selected event in the stored events array
+        const eventIndex = storedEvents.findIndex(event => event.id === selectedEvent.value.id);
+
+        if (eventIndex !== -1) {
+            // Remove the selected event from the stored events array
+            storedEvents.splice(eventIndex, 1);
 
             // Save the modified events array back to local storage
             localStorage.setItem('calendarEvents', JSON.stringify(storedEvents));
@@ -93,6 +122,7 @@ const saveChanges = () => {
             <input v-model="newTitle" />
         </label>
         <button @click="saveChanges">Save Changes</button>
+        <button @click="deleteEvent">Delete Event</button>
     </div>
 </div>
 </template>
